d791e16c25fb0bf3005f1ee58354eb1f60b0945a
monkeypineapple <davidrosenholz@gmail.com>
2020-06-10 15:49:49 -0700

    reshaped some state

--- ofcpineapple/src/reducers/deckReducer.js
+++ ofcpineapple/src/reducers/deckReducer.js
@@ -178,6 +178,43 @@ const initialDeckState = [
   },
 ];
 
+
+/*
+
+
+CardStatus = {
+
+  hand: {
+    handCards: {1: Card, 2: Card, 3: null}
+  },
+  playerOne: {
+
+    rowOne: {
+      1: null,
+      2: Card
+      // etc.
+    },
+
+    rowTwo: {
+      // same as rowOne
+    },
+
+    rowThree: {
+      // same as rowOne
+    }
+
+  },
+  playerTwo: // same as playerOne
+}
+
+
+*/
+
+
+
+
+
+
 export default function deckReducer(state = initialDeckState, action) {
   return state;
 }
