b0106b21fb40e5d4c4e6445bd4c4bbb49b2b8f72
monkeypineapple <davidrosenholz@gmail.com>
2020-06-15 11:01:03 -0700

    created cardStatus actions

--- ofcpineapple/src/actions/cardStatus.js
+++ ofcpineapple/src/actions/cardStatus.js
@@ -1,52 +1,47 @@
 import { INITIAL_DEAL, REMOVE_HAND_CARD, CLEAR_HAND, REMOVE_PLAYER_CARD, FILL_HAND, ADD_PLAYER_CARD, CLEAR_PLAYER_CARDS, GENERATE_RANDOM_BOARD } from './types.js';
 
-export const intitalDeal = () => {
+export const intitalDeal = (deck) => ({
   type: INITIAL_DEAL,
+  payload: deck.slice(0,10)
+})
+
+export const removeHandCard = (key) => ({
+  type: REMOVE_HAND_CARD,
+  payload: key
+
+})
+
+// export const clearHand = () => {
+//   type: CLEAR_HAND
+
+// }
+
+export const removePlayerCard = (info) => ({
+  type: REMOVE_PLAYER_CARD,
+  payload: info
+})
+
+export const addPlayerCard = (info) => ({
+  type: ADD_PLAYER_CARD, 
+  payload: info
+})
+
+export const fillHand = (deck) => ({
+  type: FILL_HAND,
+  payload: deck.slice(0,3)
+
+})
 
 
 
-}
+// export const clearPlayerCards = () => ({
+//   type: CLEAR_PLAYER_CARDS
 
-export const removeHandCard = () => {
-  type: REMOVE_HAND_CARD
+// })
 
-}
+// export const generateRandomBoard = () => ({
+//   type: GENERATE_RANDOM_BOARD
 
-export const clearHand = () => {
-  type: CLEAR_HAND
-
-}
-
-export const removePlayerCard = () => {
-  type: REMOVE_PLAYER_CARD
-
-}
-
-export const fillHand = () => {
-  type: FILL_HAND
-
-}
-
-export const addPlayerCard = () => {
-  type: ADD_PLAYER_CARD
-
-}
-
-export const clearPlayerCards = () => {
-  type: CLEAR_PLAYER_CARDS
-
-}
-
-export const generateRandomBoard = () => {
-  type: GENERATE_RANDOM_BOARD
-
-}
-
-
-
-export const removeHandCard = () => {
-  type: REMOVE_HAND_CARD
-
-}
+// })
 
 
