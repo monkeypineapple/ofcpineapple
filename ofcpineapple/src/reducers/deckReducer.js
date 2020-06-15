32e261b891ba411187f053f2af29e7f9ab1b6f8a
coronaisme <coronaisme@gmail.com>
2020-06-15 10:35:03 -0700

    more types

--- ofcpineapple/src/reducers/deckReducer.js
+++ ofcpineapple/src/reducers/deckReducer.js
@@ -1,32 +1,5 @@
 
 
-// var suits = ["spades", "diamonds", "clubs", "hearts"];
-// var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
-
-
-// function getDeck()
-// {
-// 	var deck = new Array();
-
-// 	for(var i = 0; i < suits.length; i++)
-// 	{
-// 		for(var x = 0; x < values.length; x++)
-// 		{
-
-      //need to add facup images and facedown images
-      
-// 			var card = {Value: values[x], Suit: suits[i]};
-// 			deck.push(card);
-// 		}
-// 	}
-
-// 	return deck;
-// }
-
-
-
-
 
 const initialDeckState = [
   {
@@ -205,9 +178,6 @@ const initialDeckState = [
     faceDownImg: "https://cdn2.f-cdn.com/contestentries/88434/10447013/53bec8a13782d_thumb900.jpg",
   },
 ];
-
-
-
 
 
 
