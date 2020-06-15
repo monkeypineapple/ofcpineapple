517b0e61784182f497c93010b83fab4951174eae
monkeypineapple <davidrosenholz@gmail.com>
2020-06-15 11:08:56 -0700

    created switch statement wireframe

--- ofcpineapple/src/reducers/cardStatusReducer.js
+++ ofcpineapple/src/reducers/cardStatusReducer.js
@@ -1,3 +1,12 @@
+import {
+  INITIAL_DEAL,
+  REMOVE_HAND_CARD,
+  CLEAR_HAND,
+  REMOVE_PLAYER_CARD,
+  FILL_HAND,
+  ADD_PLAYER_CARD,
+} from "../actions/types";
+
 const initialCardStatusState = {
   hand: {
     handCards: { 1: null, 2: null, 3: null },
@@ -54,5 +63,19 @@ export default function cardStatusReducer(
   state = initialCardStatusState,
   action
 ) {
+  switch (action.type) {
+    case INITIAL_DEAL:
+      return state;
+    case REMOVE_PLAYER_CARD:
+      return state;
+    case ADD_PLAYER_CARD:
+      return state;
+    case FILL_HAND:
+      return state;
+    case REMOVE_HAND_CARD:
+      return state;
+    case CLEAR_HAND:
+      return state;
+  }
   return state;
 }
