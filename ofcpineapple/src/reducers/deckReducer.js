122e338200fd5c27c0f9affc3b3a9f28516267e1
monkeypineapple <davidrosenholz@gmail.com>
2020-06-17 12:38:06 -0700

    loaded card imgs to initialDeckState

--- ofcpineapple/src/reducers/deckReducer.js
+++ ofcpineapple/src/reducers/deckReducer.js
@@ -5,193 +5,316 @@ import _ from "underscore";
 const initialDeckState = _.shuffle([
   {
     faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    value: 1,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/1AS.png")
   },
   {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    faceUp: true,
+    value: 2,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/2S.png")
   },
   {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    faceUp: true,
+    value: 3,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/3S.png")
   },
   {
-    faceUp: false,
+    faceUp: true,
+    value: 4,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/4S.png")
+  },
+  {
+    faceUp: true,
+    value: 5,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/5S.png")
+  },
+  {
+    faceUp: true,
+    value: 6,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/6S.png")
+  },
+  {
+    faceUp: true,
     value: 7,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/7S.png")
+  },
+  {
+    faceUp: true,
+    value: 8,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/8S.png")
+  },
+  {
+    faceUp: true,
+    value: 9,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/9S.png")
+  },
+  {
+    faceUp: true,
+    value: 10,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/10S.png")
+  },
+  {
+    faceUp: true,
+    value: 11,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/11S.png")
+  },
+  {
+    faceUp: true,
+    value: 12,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/12S.png")
+  },
+  {
+    faceUp: true,
+    value: 13,
+    suit: "spades",
+    faceUpImg: require("../assets/spades/13S.png")
+  },
+  {
+    faceUp: true,
+    value: 1,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/1AH.png")
+  },
+  {
+    faceUp: true,
+    value: 2,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/2H.png")
+  },
+  {
+    faceUp: true,
+    value: 3,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/3H.png")
+  },
+  {
+    faceUp: true,
+    value: 4,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/4H.png")
+  },
+  {
+    faceUp: true,
+    value: 5,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/5H.png")
+  },
+  {
+    faceUp: true,
+    value: 6,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/6H.png")
+  },
+  {
+    faceUp: true,
+    value: 7,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/7H.png")
+  },
+  {
+    faceUp: true,
+    value: 8,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/8H.png")
+  },
+  {
+    faceUp: true,
+    value: 9,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/9H.png")
+  },
+  {
+    faceUp: true,
+    value: 10,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/10H.png")
+  },
+  {
+    faceUp: true,
+    value: 11,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/11H.png")
+  },
+  {
+    faceUp: true,
+    value: 12,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/12H.png")
+  },
+  {
+    faceUp: true,
+    value: 13,
+    suit: "hearts",
+    faceUpImg: require("../assets/hearts/13H.png")
+  },
+  {
+    faceUp: true,
+    value: 1,
     suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    faceUpImg: require("../assets/clubs/1AC.png")
+  },
+  {
+    faceUp: true,
+    value: 2,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/2C.png")
+  },
+  {
+    faceUp: true,
+    value: 3,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/3C.png")
+  },
+  {
+    faceUp: true,
+    value: 4,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/4C.png")
+  },
+  {
+    faceUp: true,
+    value: 5,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/5C.png")
+  },
+  {
+    faceUp: true,
+    value: 6,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/6C.png")
   },
   {
     faceUp: true,
     value: 7,
     suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    faceUpImg: require("../assets/clubs/7C.png")
+  },
+  {
+    faceUp: true,
+    value: 8,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/8C.png")
+  },
+  {
+    faceUp: true,
+    value: 9,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/9C.png")
+  },
+  {
+    faceUp: true,
+    value: 10,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/10C.png")
+  },
+  {
+    faceUp: true,
+    value: 11,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/11C.png")
+  },
+  {
+    faceUp: true,
+    value: 12,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/12C.png")
+  },
+  {
+    faceUp: true,
+    value: 13,
+    suit: "clubs",
+    faceUpImg: require("../assets/clubs/13C.png")
+  },
+  {
+    faceUp: true,
+    value: 1,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/1AD.png")
+  },
+  {
+    faceUp: true,
+    value: 2,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/2D.png")
+  },
+  {
+    faceUp: true,
+    value: 3,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/3D.png")
+  },
+  {
+    faceUp: true,
+    value: 4,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/4D.png")
+  },
+  {
+    faceUp: true,
+    value: 5,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/5D.png")
+  },
+  {
+    faceUp: true,
+    value: 6,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/6D.png")
   },
   {
     faceUp: true,
     value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/7D.png")
   },
   {
     faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    value: 8,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/8D.png")
   },
   {
     faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    value: 9,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/9D.png")
   },
   {
     faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    value: 10,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/10D.png")
   },
   {
     faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    value: 11,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/11D.png")
   },
   {
     faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    value: 12,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/12D.png")
   },
   {
     faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
+    value: 13,
+    suit: "diamonds",
+    faceUpImg: require("../assets/diamonds/13D.png")
   },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: true,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  },
-  {
-    faceUp: false,
-    value: 7,
-    suit: "clubs",
-    faceUpImg: require("../assets/KS.png"),
-    faceDownImg: require("../assets/EmptySlot.png")
-  }
 ]);
 
 export default produce((state, action) => {
