e6fbda65c38722a1493f162c662ec5a6f22e0088
monkeypineapple <davidrosenholz@gmail.com>
2020-06-17 16:48:43 -0700

    changed background and card slots

--- ofcpineapple/src/index.js
+++ ofcpineapple/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './components/App/App.js'
 import store from './store.js'
-console.log(store, 'store')
+import "./index.css"
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'))
 
